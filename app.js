cf879d89493da9e801b8c6bdb29470a101a0c9e2
Daniel Espinosa <despinosa12@gmail.com>
2014-11-30 21:57:38 -0700

    Added logo, wrote about
    Created some tentative logos... wrote a bit in about

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(stylus.middleware(
 	}
 ));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
