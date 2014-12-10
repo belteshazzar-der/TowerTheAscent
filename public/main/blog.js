4726fc1ae27091697276c4bcb025a982b7fec2c2
kolipoki <ldstrumpet@gmail.com>
2014-12-10 16:06:35 -0700

    Added more changes to update page.  Adding and Editing posts now work.  Comments are now styled.  Showing and Hiding comments work.  New Posts now go to front.

--- public/main/blog.js
+++ public/main/blog.js
@@ -1,10 +1,11 @@
 var towerApp = angular.module('towerApp', []);
 towerApp.controller('towerController', function($scope) {
-  $scope.updates = [{id:'1',title:'New Monsters',date:'Jan 10, 2014',content:'10 Additional monsters were added including the marvelous superheros.  These new creatures can be found randomly through attack stuff. The new creatures are as followed:',comments:'0'},
-  {id:'2',title:'New Heros',date:'Dec 10, 2013',content:'Tons of new stuff was adding in this update.  There just isnt enough room to talk about everything that was updated.  I did just find a bug while type this out though, good to know.',comments:'1'},
-  {id:'3',title:'Game Started',date:'Nov 09, 2013',content:'On this artificially created day the game was released.  It instantly soared into popularity and became the best game of all time.',comments:'0'}]
+  $scope.updates = [{id:'1',title:'New Monsters',date:'Jan 10, 2014',content:'10 Additional monsters were added including the marvelous superheros.  These new creatures can be found randomly through attack stuff. The new creatures are as followed:',numComments:'0',comments:[],showComments:false},
+  {id:'2',title:'New Heros',date:'Dec 10, 2013',content:'Tons of new stuff was adding in this update.  There just isnt enough room to talk about everything that was updated.  I did just find a bug while type this out though, good to know.',numComments:'2', comments:["I love this update!", "So awesome!"],showComments:false},
+  {id:'3',title:'Game Started',date:'Nov 09, 2013',content:'On this artificially created day the game was released.  It instantly soared into popularity and became the best game of all time.',numComments:'0', comments:[],showComments:false}]
 
   $scope.showForm = false;
+  $scope.showTitle = false;
 
   $scope.toggleComments = function() { 
   	$scope.showComments = !$scope.showComments;
@@ -15,41 +16,44 @@ towerApp.controller('towerController', function($scope) {
   	console.log('something')
   }
 
-  $scope.editPost = function() 
+  $scope.editPost = function(update) 
   {
-  	alert("You are not the owner of this post!");
+	$scope.toggleForm();
+	$scope.newUpdate = update;
   }
 
-  $scope.showComments = function(id) 
-  {
-  	if(document.getElementById('abc').style.display == "block")
-  	{
-	  document.getElementById('abc').style.display == "none";
-  	}
-  	else
-  	{
-	  document.getElementById('abc').style.display == "block";
-  	}
+  $scope.toggleComments = function(update) { 
+  	update.showComments = !update.showComments 
   }
-
+  
   $scope.addComment = function() 
   {
   	console.log('something')
   }
 
-  $scope.check_empty = function() 
+  $scope.check_empty = function(newUpdate) 
   {
-	if (document.getElementById('title').value == "" || document.getElementById('msg').value == "") 
+	if (newUpdate == null || newUpdate.title == "" || newUpdate.content == "") 
 	{
 		alert("Fill All Fields !");
+		return
 	} 
-	else 
-	{
-		document.getElementById('form').submit();
-		alert("Form Submitted Successfully...");
-	}
+	for(var i=0;i<$scope.updates.length;i++){
+    	if($scope.updates[i].id==newUpdate.id){
+        	$scope.toggleForm();
+			alert("Updates Saved Successfully...");
+        	return;
+    	}
+    }
+	$scope.updates.unshift(newUpdate)
+	$scope.toggleForm();
+	alert("Form Submitted Successfully...");
   }
+  
   $scope.toggleForm = function(){ 
-  	$scope.showForm = !$scope.showForm
+  	$scope.showForm = !$scope.showForm;
+  	if($scope.showForm) {
+  		$scope.newUpdate = {id:'NA',title:'',date:new Date().toDateString(),content:'',numComments:'0', comments:[], showComments:false};
+  	}
   }
 });
